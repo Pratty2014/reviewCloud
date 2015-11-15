d83d247814bbe274a58412ea18a2a09417bfecac
katrina <katrina.s.liao@gmail.com>
2015-11-14 20:22:10 -0500

    added pages;

--- client/app/ReviewFactory/reviewFactory.service.js
+++ client/app/ReviewFactory/reviewFactory.service.js
@@ -3,6 +3,22 @@
 angular.module('reviewCloudApp')
   .factory('reviewFactory', function () {
     return {
-      
+      hotels: [
+      	{
+      		name : 'Ace Hotel',
+      		price: "$500",
+      		path: '/hotel-reviews-mgmgram.json'
+  		}, 
+  		{
+  			name: 'Hotel Penn',
+  			price: "$100",
+  			path: '/hotel-reviews-hotelpenn.json'
+  		}, 
+  		{
+  			name: 'Marriot Marquis',
+  			price: "$400",
+  			path: '/hotel-reviews-marriot-marquis.json'
+  		}
+  		]
     };
   });
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,6 +2,19 @@
 
 angular.module('reviewCloudApp')
   .controller('MainCtrl', function($scope, $http, socket, $interval, $timeout, reviewFactory) {
+    $scope.first = true;
+    $scope.second = false;
+    $scope.third = false;
 
+    $scope.search = function(){
+      $scope.first = false;
+      $scope.second = true;
+    }
+
+    $scope.details = function(hotel){
+      $scope.hotelInfo = hotel;
+    }
+
+    $scope.hotels = reviewFactory.hotels;
 
   });
